f99eee8ab227bc546c3b6b8d51135fd9880a4f2b
ZhouGuangyuan <zhouguangyuan@golangcn.org>
2025-11-18 23:32:56 +0800

    Add more binding functions

---
+++ goallc_ext.go
@@ -0,0 +1,29 @@
+package llvm
+
+/*
+#include "llvm-c/Core.h"
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"errors"
+	"unsafe"
+)
+
+func LLVMPrintModuleToFile(module Module, filename string) error {
+	filenamestr := C.CString(filename)
+	defer C.free(unsafe.Pointer(filenamestr))
+	var errmsg *C.char
+	if C.LLVMPrintModuleToFile(module.C, filenamestr, &errmsg) != 0 {
+		err := errors.New(C.GoString(errmsg))
+		C.LLVMDisposeMessage(errmsg)
+		return err
+	}
+	return nil
+}
+
+func LLVMSetValueName2(value Value, name string) {
+	namestr := C.CString(name)
+	defer C.free(unsafe.Pointer(namestr))
+	C.LLVMSetValueName2(value.C, namestr, C.size_t(len(name)))
+}
